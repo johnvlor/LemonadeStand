463dcdd2569090f97fb23d41b1bceab3423bd728
John Lor <j_lor@hotmail.com>
2017-07-12 08:27:30 -0500

    add initial classes and player input

---
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        Player playerOne;
+
+        public Game()
+        {
+
+        }
+
+        public void PlayGame()
+        {
+            GetPlayer();
+        }
+
+        public void GetPlayer()
+        {
+            playerOne = new Human("player one");
+
+        }
+    }
+}
---
+++ LemonadeStand/LemonadeStand/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Human : Player
+    {
+        public Human(string name)
+        {
+            this.name = name;
+            ChangeName();
+        }
+
+        public void ChangeName()
+        {
+            Console.WriteLine("Enter {0}'s name: ", name);
+            name = Console.ReadLine();
+        }
+    }
+}
